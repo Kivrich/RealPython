07526248f1c20a93b3ba860c506cba3eaa01e187
Rich W <richard.westvik@gmail.com>
2019-09-27 14:15:26 -0700

    Covered more database topics

--- sql/02_sql.py
+++ sql/02_sql.py
@@ -1,4 +1,9 @@
+### This is not the original 02 ###
+
 # Inserting Data
+
+# Refactored the code to include error handling
+# changed population to populations to trigger error 
 
 # import SQLite3 library
 import sqlite3
@@ -10,14 +15,18 @@ conn = sqlite3.connect("new.db")
 # create cursor object
 cursor = conn.cursor()
 
-# execute sql commands with cursor to insert data
-cursor.execute("INSERT INTO population VALUES('New York City', \
-    'NY', 8400000)")
-cursor.execute("INSERT INTO population VALUES('San Francisco', \
-    'CA', 8000000)")
+# Try
+try:
+    # execute sql commands with cursor to insert data
+    cursor.execute("INSERT INTO populations VALUES('New York City', \
+        'NY', 8400000)")
+    cursor.execute("INSERT INTO populations VALUES('San Francisco', \
+        'CA', 8000000)")
 
-# commit changes. New command. Do this to save changes before closing connection
-conn.commit()
+    # commit changes. New command. Do this to save changes before closing connection
+    conn.commit()
+except:
+    print("Ooops! Something went wrong, check your code and try again.")
 
 # close the database connection
 conn.close()
